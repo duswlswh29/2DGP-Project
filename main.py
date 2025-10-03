14175c2520d9ddd7ca3b4834ea73364f836fdc65
duswlswh29 <yeonjin1029@naver.com>
2025-10-03 15:50:07 +0900

    idle 모션 출력 후 위치 조정

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
 from pico2d import *
 
 
+#Game object class here
+class Background:
+   def __init__(self):
+       self.image=load_image('background.png') #배경이미지 로드
+
+
+
+   def draw(self):
+
+       self.image.draw(400,300,800,600)
+   def update(self):
+
+       pass
+
+class Chunli:
+   #캐릭터가 위치할 캔버스 좌표 설정
+    def __init__(self,x,y):
+        self.x=90
+        self.y=100
+        self.frame=0
+        self.image=load_image('chunli-idle1.png')
+
+        self.w=120 #캔버스에 그릴 너비
+        self.h=200 #캔버스에 그릴 높이
+
+    def update(self):
+        self.frame=(self.frame+1) % 4
+        pass
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 90, 0, 90,180, self.x, self.y)
+
+
 def handle_events():
    global running
    events=get_events()
@@ -16,17 +49,21 @@ pass
 def reset_world():
   global running
   running=True
-
+  global background1
+  background1=Background() #Background 도장을 이용해서 background1 객체생성
+  global chunli
+  chunli=Chunli(90,100)#Chunli 도장을 이용해서 chunli 객체생성
 pass
 
 def update_world():
-
+    chunli.update()
     pass
 
 def render_world():
 
     clear_canvas()
-    # .draw() 호출하고 클래스에 드로우 정의
+    background1.draw()# .draw() 호출하고 클래스에 드로우 정의
+    chunli.draw()
     update_canvas()
 pass
 open_canvas()
@@ -36,5 +73,5 @@ while running:
     handle_events() #입력 처리
     update_world() #게임 로직 업데이트
     render_world() #렌더링
-    delay(0.05) #프레임 제어
+    delay(0.1) #프레임 제어
 close_canvas()
